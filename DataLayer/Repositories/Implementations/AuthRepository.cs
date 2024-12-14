8800c0c2f5c17c3320804fc75de51158efacdb8d
Rainfront <chudoha@gmail.com>
2024-12-14 18:48:42 +0100

    Added creation of user group

--- DataLayer/Repositories/Implementations/AuthRepository.cs
+++ DataLayer/Repositories/Implementations/AuthRepository.cs
@@ -12,18 +12,21 @@ public class AuthRepository : IAuthRepository
 {
     private readonly PasswordHasher _passwordHasher;
     private readonly IUserRepository _userRepository;
+    private readonly IGroupRepository _groupRepository;
     private readonly IRefreshTokenRepository _refreshTokenRepository;
     private readonly DataContext _dataContext;
     private readonly DataGenerator _dataGenerator;
-    
+
     public AuthRepository(
         IUserRepository userRepository,
+        IGroupRepository groupRepository,
         PasswordHasher passwordHasher,
         DataContext dataContext,
         IRefreshTokenRepository refreshTokenRepository,
         DataGenerator dataGenerator)
     {
         _userRepository = userRepository;
+        _groupRepository = groupRepository;
         _passwordHasher = passwordHasher;
         _dataContext = dataContext;
         _refreshTokenRepository = refreshTokenRepository;
@@ -35,13 +38,25 @@ public class AuthRepository : IAuthRepository
         string hash, salt;
         (hash, salt) = await _passwordHasher.CreatePasswordHashAsync(registerDto.Password);
         User user = new()
-            { Login = registerDto.Login, 
-                Email = registerDto.Email, PasswordHash = hash, PasswordSalt = salt, RegistrationDate = DateTime.UtcNow };
+        {
+            Login = registerDto.Login!,
+            Email = registerDto.Email, 
+            PasswordHash = hash, 
+            PasswordSalt = salt, 
+            RegistrationDate = DateTime.UtcNow
+        };
 
         await _userRepository.AddUserAsync(user);
+        Group userGroup = new()
+        {
+            IsUserGroup = true,
+            CreationDate = DateTime.UtcNow,
+            Title = user.Login
+        };
+        await _groupRepository.AddGroupAsync(userGroup);
         return user;
     }
-    
+
     public async Task<(JwtToken, JwtToken)> LoginAsync(LoginProp prop)
     {
         var userForLogin = await _userRepository.GetUserByCredentials(prop.LoginOrEmail);
@@ -50,19 +65,21 @@ public class AuthRepository : IAuthRepository
             throw new Exception("Bad credentials");
         }
 
-        bool isPasswordRight = await _passwordHasher.VerifyPasswordHashAsync(prop.Password, userForLogin.PasswordHash, userForLogin.PasswordSalt);
+        bool isPasswordRight =
+            await _passwordHasher.VerifyPasswordHashAsync(prop.Password, userForLogin.PasswordHash,
+                userForLogin.PasswordSalt);
         if (!isPasswordRight)
             throw new Exception("Bad credentials");
-        
+
         var accessJwtToken = _passwordHasher.GenerateToken(userForLogin);
         var refreshJwtToken = _passwordHasher.GenerateToken(userForLogin, true);
-        
+
         // save refresh token for user
         var refreshToken = refreshJwtToken.JwtToRefreshToken();
 
         refreshToken.UserId = userForLogin.UserId.Value;
         await _refreshTokenRepository.AddRefreshTokenAsync(refreshToken);
-        
+
         return (accessJwtToken, refreshJwtToken);
     }
 
@@ -79,6 +96,7 @@ public class AuthRepository : IAuthRepository
         {
             throw new Exception("User id in jwt is invalid");
         }
+
         var userFromJwt = await _userRepository.GetUserAsync(userIdFromJwt);
         if (userFromJwt.RefreshToken.Token != refreshToken)
         {
@@ -95,7 +113,7 @@ public class AuthRepository : IAuthRepository
         var generatedRefreshToken = generatedRefreshJwtToken.JwtToRefreshToken();
         generatedRefreshToken.User = userFromJwt;
         generatedRefreshToken.UserId = userFromJwt.UserId.Value;
-        
+
         await _refreshTokenRepository.AddRefreshTokenAsync(generatedRefreshToken);
 
         generatedRefreshToken.User.RefreshToken = null;
@@ -112,8 +130,7 @@ public class AuthRepository : IAuthRepository
             createdUser = await RegisterUserAsync(registerDto);
             addedUsers.Add(createdUser);
         }
+
         return addedUsers;
     }
-
-
 }
